f88d15c1f61bc82365701bc63006748d6c36f791
bibhuranjan <bib.mhnt@gmail.com>
2023-12-08 18:16:28 +0530

    Data Binding

--- 04-12-2023/my-app/src/app/app.component.ts
+++ 04-12-2023/my-app/src/app/app.component.ts
@@ -1,19 +1,49 @@
 import { Component } from '@angular/core';
+import { defaultUrlMatcher } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // template: '<h1>{{title}}</h1>',
   templateUrl:'./app.component.html',
-  styles:[`p{color:blue;font-size:20px}`,
-          `h1{color:red}`,
-          `.d1{width:500px;}`
-        ],
-  styleUrls:['./app.component.css',
-            '../assets/css/mystyle.css'
+  styleUrls:[
+              './app.component.css',
+            
             ],
 })
 export class AppComponent {
-  title = 'Welcome to Angular';
-  style1 :string ='c3';
-  style2=['c4','c5'];
+  
+  classToApply :string ='italicClass boldClass';
+  IsBold:boolean = false;
+  IsItalic:boolean = true;
+  txtColor:boolean=true;
+  AddCSSClasses(){
+     let obj ={
+      boldClass :this.IsBold,
+      italicClass:this.IsItalic,
+      txtColor:this.txtColor
+     }
+     return obj;
+  }
+  clicked :string="Not Clicked"
+  show(){
+    this.clicked="Now Button Clicked"
+  }
+  data:any="sample"
+
+  
+//  firstName :string = "Rajib";
+//  name :string="<h1>dshghdghdgshdsf</h1>"
+//  lastName :any="Kumar";
+//  salary :number=50000;
+//  IsDisabledClick:boolean=false;
+//  MaliciousData : string = "Hello <script>alert('your application is hacked')</script>";
+
+//  getFullName(){
+//      return this.firstName +' '+this.lastName;
+//  }
+
+//  img:string ="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=";
+
+//  localImg:string="../assets/images/1.jpg";
 }
+
+
--- 04-12-2023/my-app/src/app/app.module.ts
+++ 04-12-2023/my-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { EmployeesComponent } from './employees/employees.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +15,13 @@ import { EmployeesComponent } from './employees/employees.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
